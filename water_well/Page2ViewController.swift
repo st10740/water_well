faa85b1483173ad069a8bef267c206c355a69777
ivywang <ivywang2015@gmail.com>
2020-07-14 11:09:13 +0800

    進度條完成

--- water_well/Page2ViewController.swift
+++ water_well/Page2ViewController.swift
@@ -10,8 +10,45 @@ import UIKit
 
 class Page2ViewController: UIViewController {
 
+    
+    var line = UIProgressView()
+    let WIDTH = UIScreen.main.bounds.size.width
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //MARK: gradient view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.init(displayP3Red: 21/255, green: 59/255, blue: 217/255, alpha: 1).cgColor, UIColor.init(displayP3Red: 60/255, green: 111/255, blue: 255/255, alpha: 1).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        //gradientLayer.locations = [0, 0.3]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+
+        
+        line = UIProgressView(progressViewStyle: .default)
+        //line.frame.size = CGSize(width: 160, height:200)
+        line.center = CGPoint(x: WIDTH / 2,
+                              y: 400)
+        line.progress = 0.5
+        line.progressTintColor = .blue
+        line.trackTintColor = .white
+        
+        //設定progressview的長寬
+        let transform : CGAffineTransform = CGAffineTransform(scaleX: 1.2, y: 3.0)
+        line.transform = transform
+        
+        //設定圓角
+        line.clipsToBounds = true
+        line.layer.cornerRadius = 5
+        
+        //設定邊框
+        line.layer.borderColor = UIColor.white.cgColor
+        line.layer.borderWidth = 0.3
+        
+        view.addSubview(line)
+        
         
         
     }
